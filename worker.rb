b5a8527a2ece055d85dff07c20455b4b8b4f8e2a
Kyle Fritz <kyle.p.fritz@gmail.com>
2011-09-24 05:56:47 -0400

    dummy, index!

--- worker.rb
+++ worker.rb
@@ -7,10 +7,10 @@ REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.passwor
 
 
 loop do
-  key,text=REDIS.blpop("screenhead:text",5)
-  puts text
-  unless text.nil?
-    json = JSON.load(open("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=1&q=#{URI.escape(text)}"))
+  resp=REDIS.blpop("screenhead:text",5)
+  p resp
+  unless resp.nil?
+    json = JSON.load(open("https://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=1&q=#{URI.escape(resp[1])}"))
     img_url=json['responseData']['results'][0]['unescapedUrl']
     `open #{img_url}`
   end
