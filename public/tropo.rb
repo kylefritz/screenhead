698e006fbdb6e6fb2c32fa6061f9441824689c05
Kyle Fritz <kyle.p.fritz@gmail.com>
2011-09-24 05:47:48 -0400

    dont pass hash to http post

--- public/tropo.rb
+++ public/tropo.rb
@@ -6,9 +6,9 @@ say "ok, #{text} comming right up..."
 
 http = Net::HTTP.new('screenhead.heroku.com')
 
-http.post "/text" , {"text"=> text }
+http.post "/text" , "text=#{text}"
 
 if text =~ /launch/i
-  http.post "/launch", {}
+  http.post "/launch", ""
   say "oh snap, now you're really in trouble...."
 end
